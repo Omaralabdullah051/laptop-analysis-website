ac332b36d2c2e59e5af8ab3af96e83e35c3dac2f
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-04-04 10:30:15 +0400

    all questions added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Question1 from '../Question1/Question1';
+import Question2 from '../Question2/Question2';
+import Question3 from '../Question3/Question3';
 
 const Blogs = () => {
     return (
         <div>
             <Question1></Question1>
+            <Question2></Question2>
+            <Question3></Question3>
         </div>
     );
 };
---
+++ src/components/Question2/Question2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Question2 = () => {
+    return (
+        <div className='mt-10'>
+            <h3 className='text-cyan-500 font-bold text-center'>What is Semantic tag?</h3>
+            <h5 className='font-semibold p-6 text-justify'>In Html, there are many tags for different purposes. Semantic tag is one of them. The most important thing of semantic tag is discription. It is a well descripted tag that clearly describes its purpose and content. It is a meaningful tag and the browser and developer cleary understand the purpose of the content. Non-semanctic tags are div, span, etc. These tags have no meaning. On the other hand, the header, nav, main, section, article, aside, footer tag is called semantic tag. These tag represents the importance of the context of the web pages. The semantic tag made the pages much more easier to read. It gives better accessibility and finally it offeres a better user experience.</h5>
+        </div>
+    );
+};
+
+export default Question2;
