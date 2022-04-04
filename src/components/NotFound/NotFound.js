e6c60431cd489a0df5f81cc2664279923897f160
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-04-04 09:17:14 +0400

    added 404 route & webpage title

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className='mt-10'>
+            <h3 className='text-red-700 font-bold text-center'>Opps!404...</h3>
+            <h5 className='text-red-500 font-semibold text-center'>Not available!!!</h5>
+        </div>
+    );
+};
+
+export default NotFound;
